3fc5a26317a186c3e57f933dbab7e892baf43f13
ArtemyevArtyom <gof.ru@yandex.ru>
2022-10-20 14:14:25 +0400

    Homework 5

---
+++ task36/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int sumOfElementsInOddPositions(int[] incomingArray)
+{
+    int count = 0;
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count += incomingArray[i];
+        }
+    }
+    return count;
+
+}
+
+int[] currentArray = getRandomArray(4, -100, 100);
+printArray(currentArray);
+
+int sumOfElements = sumOfElementsInOddPositions(currentArray);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sumOfElements}");
---
+++ task38/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+double[] getRandomArray(int length, double startPoint, double endPoint)
+{
+    double[] resultArray = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return resultArray;
+}
+
+void DifferenceBetweenMaxAndMin(double[] incomingArray)
+{
+    double minPosition = incomingArray[0];
+    double maxPosition = incomingArray[0];
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        if (minPosition > incomingArray[i])
+        {
+            minPosition = incomingArray[i];
+        }
+        else if (maxPosition < incomingArray[i])
+        {
+            maxPosition = incomingArray[i];
+        }
+    }
+    double result = maxPosition - minPosition;
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {result}");
+}
+
+Console.WriteLine("Введите массив вещественных чисел:");
+double[] currentArray = getRandomArray(4, -100, 100);
+
+DifferenceBetweenMaxAndMin(currentArray);
+
